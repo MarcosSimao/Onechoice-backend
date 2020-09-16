4c563e4fb27e1b69afee55c51618a305963308a9
MarcosSimao <marcosjuremao@hotmail.com>
2020-09-16 20:47:21 -0300

    project created

---
+++ src/main/java/com/onechoice/start/OnechoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onechoice.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnechoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnechoiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onechoice/start/OnechoiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onechoice.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnechoiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
